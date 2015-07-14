d11dd25a49d123c64f7acfe4d883513f0a8b93cf
songjie <songjiesdnu@163.com>
2015-07-14 18:48:51 +0800

    项目初始化

---
+++ src/main/java/com/robot/turnip/service/IUserService.java
@@ -0,0 +1,15 @@
+/**
+ * 
+ */
+package com.robot.turnip.service;
+
+import com.robot.turnip.domain.User;
+
+/**
+ * @author songjie
+ *
+ */
+public interface IUserService {
+	public User getUserById(int userId);
+}
+
